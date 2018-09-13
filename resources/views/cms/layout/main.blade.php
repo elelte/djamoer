11ec4aefd8b1fe90863ac11a03db1f6a863e3f59
elelte <ganalpratama@gmail.com>
2018-09-13 23:49:41 +0700

    update

--- resources/views/cms/layout/main.blade.php
+++ resources/views/cms/layout/main.blade.php
@@ -6,10 +6,34 @@
         @yield('header')
     </head>
 
-    <body>
+    <body class="hold-transition skin-blue sidebar-mini">
+        <div class="wrapper">
+            @include('cms/layout/mainheader')
+            <!-- Left side column. contains the logo and sidebar -->
+            @include('cms/layout/sidebar')
 
-        @yield('content')
+            <!-- Content Wrapper. Contains page content -->
+            <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <h1>
+                Dashboard
+                <small>Control panel</small>
+                </h1>
+                <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+                <li class="active">Dashboard</li>
+                </ol>
+            </section>
 
-        @include('cms/layout/header')
+            <!-- Main content -->
+            <section class="content">
+                @yield('content')
+            </section>
+            <!-- /.content -->
+            </div>
+            @yield('footer')
+            @include('cms/layout/footer')
+        </div>
     </body>
 </html>
