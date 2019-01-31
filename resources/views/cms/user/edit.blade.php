c9c8b756430bd112a4b3e1a29c3937b204783d76
Rachmad Hadi <36755261+ShiroeART@users.noreply.github.com>
2019-02-01 03:42:07 +0700

    Edit data

---
+++ resources/views/cms/user/edit.blade.php
@@ -0,0 +1,124 @@
+@extends('cms.layout.main')
+
+@section('header')
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 2 | Dashboard</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="{{asset('AdminLTE/bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{asset('AdminLTE/bower_components/font-awesome/css/font-awesome.min.css')}}">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="{{asset('AdminLTE/bower_components/Ionicons/css/ionicons.min.css')}}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{asset('AdminLTE/dist/css/AdminLTE.min.css')}}">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="{{asset('AdminLTE/dist/css/skins/_all-skins.min.css')}}">
+  <!-- Morris chart -->
+  <link rel="stylesheet" href="{{asset('AdminLTE/bower_components/morris.js/morris.css')}}">
+  <!-- jvectormap -->
+  <link rel="stylesheet" href="{{asset('AdminLTE/bower_components/jvectormap/jquery-jvectormap.css')}}">
+  <!-- Date Picker -->
+  <link rel="stylesheet" href="{{asset('AdminLTE/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css')}}">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="{{asset('AdminLTE/bower_components/bootstrap-daterangepicker/daterangepicker.css')}}">
+  <!-- bootstrap wysihtml5 - text editor -->
+  <link rel="stylesheet" href="{{asset('AdminLTE/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css')}}">
+@endsection
+
+@section('content') 
+
+<div class="box box-info">
+            <div class="box-header with-border" style="text-align:center;">
+              <h3 class="box-title">(Edit) Data</h3>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form class="form-horizontal">
+              <div class="box-body">
+                <div class="form-group">
+                  <label for="inputUsername3" class="col-sm-2 control-label">Username</label>
+
+                  <div class="col-sm-3">
+                    <input type="Username" class="form-control" id="inputUsername3" placeholder="Username">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+
+                  <div class="col-sm-3">
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+
+                  <div class="col-sm-3">
+                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="inputTelepon3" class="col-sm-2 control-label">Telepon</label>
+
+                  <div class="col-sm-3">
+                    <input type="telepon" class="form-control" data-inputmask="&quot;mask&quot;: &quot;(999) 999-9999&quot;" data-mask=""
+                     id="inputTelepon3" placeholder="Telepon">
+                  </div>
+                </div>
+                         
+              </div>
+              <!-- /.box-body -->
+              <div class="box-footer">
+                <a href=""><button type="submit" class="btn btn-default">Batal</button></a>
+                <a href=""><button type="submit" class="btn btn-info pull-right">Simpan</button></a>
+              </div>
+              <!-- /.box-footer -->
+            </form>
+          </div>
+            
+
+@endsection
+
+
+@section("footer")
+  <!-- jQuery 3 -->
+  <script src="{{asset('AdminLTE/bower_components/jquery/dist/jquery.min.js')}}"></script>
+  <!-- jQuery UI 1.11.4 -->
+  <script src="{{asset('AdminLTE/bower_components/jquery-ui/jquery-ui.min.js')}}"></script>
+  <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+  <script>
+    $.widget.bridge('uibutton', $.ui.button);
+  </script>
+  <!-- Bootstrap 3.3.7 -->
+  <script src="{{asset('AdminLTE/bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+  <!-- Morris.js charts -->
+  <script src="{{asset('AdminLTE/bower_components/raphael/raphael.min.js')}}"></script>
+  <script src="{{asset('AdminLTE/bower_components/morris.js/morris.min.js')}}"></script>
+  <!-- Sparkline -->
+  <script src="{{asset('AdminLTE/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js')}}"></script>
+  <!-- jvectormap -->
+  <script src="{{asset('AdminLTE/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js')}}"></script>
+  <script src="{{asset('AdminLTE/plugins/jvectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
+  <!-- jQuery Knob Chart -->
+  <script src="{{asset('AdminLTE/bower_components/jquery-knob/dist/jquery.knob.min.js')}}"></script>
+  <!-- daterangepicker -->
+  <script src="{{asset('AdminLTE/bower_components/moment/min/moment.min.js')}}"></script>
+  <script src="{{asset('AdminLTE/bower_components/bootstrap-daterangepicker/daterangepicker.js')}}"></script>
+  <!-- datepicker -->
+  <script src="{{asset('AdminLTE/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js')}}"></script>
+  <!-- Bootstrap WYSIHTML5 -->
+  <script src="{{asset('AdminLTE/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js')}}"></script>
+  <!-- Slimscroll -->
+  <script src="{{asset('AdminLTE/bower_components/jquery-slimscroll/jquery.slimscroll.min.js')}}"></script>
+  <!-- FastClick -->
+  <script src="{{asset('AdminLTE/bower_components/fastclick/lib/fastclick.js')}}"></script>
+  <!-- AdminLTE App -->
+  <script src="{{asset('AdminLTE/dist/js/adminlte.min.js')}}"></script>
+  <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+  <script src="{{asset('AdminLTE/dist/js/pages/dashboard.js')}}"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="{{asset('AdminLTE/dist/js/demo.js')}}"></script>
+@endsection
